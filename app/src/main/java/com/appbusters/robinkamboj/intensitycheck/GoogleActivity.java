37acb3e23bf8177325928c112ac3186e8d0fe6e6
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-10 19:42:48 +0530

    Firebase Auth.

--- app/src/main/java/com/appbusters/robinkamboj/intensitycheck/GoogleActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/intensitycheck/GoogleActivity.java
@@ -1,13 +1,61 @@
 package com.appbusters.robinkamboj.intensitycheck;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class GoogleActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class GoogleActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+    //Firebase instance variables
+    private FirebaseAuth firebaseAuth;
+    private SignInButton signInButton;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google);
+
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        googleApiClient = new GoogleApiClient.Builder(GoogleActivity.this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        //Initialize FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn(){
+        Intent i = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(i, 9001);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "PLAY SERVICES ERROR.", Toast.LENGTH_SHORT).show();
     }
 }
